2d167d89cfc430b19ed2192f126d428d29509ac1
kizuyoko <kizuyoko@gmail.com>
2024-01-23 15:50:59 +0100

    useLayout

--- src/person-editor/usePerson.ts
+++ src/person-editor/usePerson.ts
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react"
 import localforage from "localforage"
 
 import type { Person } from "../types/person"
+import { sleep } from "../utils"
+import { useIsMounted } from "../hooks/useIsMounted"
 
 function savePerson(person: Person | null): void {
   console.log("Saving", person)
@@ -10,14 +12,19 @@ function savePerson(person: Person | null): void {
 
 export function usePerson(initialPerson: Person) {
   const [person, setPerson] = useState<Person | null>(null)
+  const isMounted = useIsMounted()
 
   useEffect(() => {
     const getPerson = async () => {
       const person = await localforage.getItem<Person>("person")
+      await sleep(50)
+      if (isMounted.current) {
+        setPerson(person ?? initialPerson)
+      }
       setPerson(person ?? initialPerson)
     }
     getPerson()
-  }, [initialPerson])
+  }, [initialPerson, isMounted])
 
   useEffect(() => {
     savePerson(person)
--- src/rules-of-hooks/Counter.tsx
+++ src/rules-of-hooks/Counter.tsx
@@ -1,14 +1,28 @@
-import React, { ReactElement, useEffect, useRef, useState } from "react"
+import React, {
+  ReactElement,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react"
 
 export function Counter(): ReactElement {
   const [counter, setCounter] = useState(0)
-  const [focus, setFocus] = useState(false)
+  //const [focus, setFocus] = useState(false)
   const button = useRef<HTMLButtonElement>(null)
 
   useEffect(() => {
     setTimeout(() => {
       button.current?.focus()
     }, 1000)
+  }, [])
+
+  // useEffect - Renders after component. The button shows blue in the first sequence, and then green.
+  // useLayoutEffect - Renders before component. The button shows green from the beggining..
+  useLayoutEffect(() => {
+    if (button.current) {
+      button.current.style.backgroundColor = "green"
+    }
   }, [])
 
   return (
