4232dd9f8f5f3749980d6ddc286e17005fa91ec5
kizuyoko <kizuyoko@gmail.com>
2024-01-25 14:16:17 +0100

    Context, provider, and values

--- src/kimrof-user-editor/KimrofUserEditor.tsx
+++ src/kimrof-user-editor/KimrofUserEditor.tsx
@@ -5,7 +5,14 @@ import React, { ReactElement } from "react"
 
 // Kimrof = Formik reversed :-)
 import { UserEditor } from "./UserEditor"
+import { Kimrof } from "./kimrof/Kimrof"
+import { initialPerson } from "../utils"
+import { IndexedPerson } from "../types/IndexedPerson"
 
 export function KimrofUserEditor(): ReactElement {
-  return <UserEditor />
+  return (
+    <Kimrof initialValues={initialPerson as IndexedPerson}>
+      <UserEditor />
+    </Kimrof>
+  )
 }
